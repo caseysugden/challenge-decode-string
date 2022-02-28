fbca7d575cf1df607278e08fb5d92313731243e4
Casey Sugden <casey.sugden@gmail.com>
2022-02-28 10:54:02 -0700

    Solved challenge

--- decodeString.js
+++ decodeString.js
@@ -1,1 +1,47 @@
 // Write your code below this line
+let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+];
+
+function decodeString(string) {
+    let arr = string.split('');
+    let num = +arr.shift();
+    for (let i =0; i < arr.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (arr[i] === alphabet[j]) {
+                if (alphabet[j + num] <= 26) {
+                    console.log(alphabet[j + num]);
+                } else {
+                    console.log(alphabet[(j + num) % 26]);
+                };
+            };
+        };
+    };
+};
+
+decodeString("1xyz");
